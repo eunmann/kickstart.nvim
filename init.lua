d9760fb3fdb0fe86ff8f123adc991eeedda0237c
Damjan 9000 <damjan.9000@gmail.com>
2024-04-17 21:25:54 +0200

    Move plugin examples from README to optional plugin files (#831)
    * Move autopairs example from README to an optional plugin
    
    * Move neo-tree example from README to an optional plugin

--- init.lua
+++ init.lua
@@ -860,9 +860,17 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
+<<<<<<< HEAD
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
+=======
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+>>>>>>> 5e258d2 (Move plugin examples from README to optional plugin files (#831))
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
